d5b36036ac9cefb015e2b174fa87b155846add88
Olulana Emmanuel <emmanuelolulana72@gmail.com>
2025-10-16 12:06:04 +0100

    FEAT: Added some so new tables to the database

--- FINDEV/DEV/models.py
+++ FINDEV/DEV/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.description}: {self.amount}"
+    
+
+class TransactionType(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    Transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='types')
+
+    def __str__(self):
+        return self.name
--- FINDEV/DEV/views.py
+++ FINDEV/DEV/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+def home(request):
+
+    transactions = models.Transaction.objects.all()
+
+    context = {
+        'transactions': transactions
+    }
+
+    return render(request, 'index.html', context=context)
